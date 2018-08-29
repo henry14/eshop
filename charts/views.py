cb044621bca93a3b40e6d5bfd3f08435c2d61e7c
henry14 <henrytebs@gmail.com>
2018-08-29 17:52:11 +0300

    Fix graph

--- charts/views.py
+++ charts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.db.models import Sum 
 from estore.models import Product
+from orders.models import OrderItem
+from django.db.models.functions import Coalesce
 
 app_name = "charts"
 
 def chart_data(request):
-	dataset = Product.objects.values('name','stock').annotate(stock_count=Sum('stock')).order_by('stock')
-	return render(request, 'charts/graph.html', {'dataset':dataset})
+	product_dataset = Product.objects.values('name','stock') \
+	    .annotate(stock_sum=Sum('stock')).order_by('name')
+
+	item_dataset = OrderItem.objects.values('product__name','quantity') \
+	    .annotate(item_sum=Coalesce(Sum('quantity'), 0)).order_by('product__name')
+
+	return render(request, 'charts/graph.html',{'item_dataset':item_dataset, 'product_dataset':product_dataset})
 
